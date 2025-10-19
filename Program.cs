f7e23dc2b5959eef7e3b6ca480ac1b27b22fee3b
Jamie  Mckenzie <235662121+jamiemckenziejm-png@users.noreply.github.com>
2025-10-19 13:51:57 +0100

    adding the repositary to github

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CET1004_Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
