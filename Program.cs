26eb10183f5d7d7dddddd9dbe23a299ff51b7d26
Jamie  Mckenzie <235662121+jamiemckenziejm-png@users.noreply.github.com>
2025-10-23 14:55:46 +0100

    rules clean up
    removed writelines and replaced with \n

--- Program.cs
+++ Program.cs
@@ -19,10 +19,16 @@ namespace CET1004_Assignment1
         static void GetUserName()
         {
             Console.Write("Please enter your name: ");
+
+            // Create Player Object
             Player_name Player1 = new Player_name(Console.ReadLine());
             Console.WriteLine();
+
+            // Welcome Message calling player name from Player Object
             Console.WriteLine("Hello " + Player1.GetPlayerName() + ", Welcome to the DICE BATTLE GAME!! ");
             Console.WriteLine();
+
+            // obtain input from user before clering the console and moving to the next method
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
             Console.Clear();
@@ -30,23 +36,15 @@ namespace CET1004_Assignment1
         static void DisplayGameRules()
         {
             Console.WriteLine("GAME RULES:");
-            Console.WriteLine("1. The game is played between two players (Player A = you : Player B = the computer) over 3 rounds");
-            Console.WriteLine();
-            Console.WriteLine("2. Each player takes turns to roll two dice.");
-            Console.WriteLine();
-            Console.WriteLine("3. Player A will choose each round to 'STICK' with the score they have or to 'RE-ROLL' the dice with the lowest value.");
-            Console.WriteLine();
-            Console.WriteLine("4. Player B will always 'RE-ROLL' if there score is less than 6 and always 'STICK' if higher.");
-            Console.WriteLine();
-            Console.WriteLine("5. Each roll for both players will be recorded inlcuding a Total Score.");
-            Console.WriteLine();
-            Console.WriteLine("6. Results will be displaed at the end of the game.");
-            Console.WriteLine();
-            Console.WriteLine("7. The player with the highest Total Score after 3 rounds wins the game.");
-            Console.WriteLine();
-            Console.WriteLine("8. In The event of a tie, the player whole rolled the most 6's will be declared the winner.");
-            Console.WriteLine();
-            Console.WriteLine("Press any key to start the game...");
+            Console.WriteLine("\n 1. The game is played between two players (Player A = you : Player B = the computer) over 3 rounds");
+            Console.WriteLine("\n 2. Each player takes turns to roll two dice.");
+            Console.WriteLine("\n 3. Player A will choose each round to 'STICK' with the score they have or to 'RE-ROLL' the dice with the lowest value.");
+            Console.WriteLine("\n 4. Player B will always 'RE-ROLL' if there score is less than 6 and always 'STICK' if higher.");
+            Console.WriteLine("\n 5. Each roll for both players will be recorded inlcuding a Total Score.");
+            Console.WriteLine("\n 6. Results will be displaed at the end of the game.");
+            Console.WriteLine("\n 7. The player with the highest Total Score after 3 rounds wins the game.");
+            Console.WriteLine("\n 8. In The event of a tie, the player whole rolled the most 6's will be declared the winner.");
+            Console.WriteLine("\n\n\n PRESS ANY KEY TO START THE GAME...");
             Console.ReadKey();
             Console.Clear();
         }
